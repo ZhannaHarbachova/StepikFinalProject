b8092ecfc669e926bda3a5c156afb2feba7e3f1e
Zhanna <gorbacheva.zh.v@gmail.com>
2022-03-13 19:26:54 +0300

    Add parametrize

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_SUM = (By.CSS_SELECTOR, ".alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def should_be_success_message(self):
+        assert self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE), "No message on the page"
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_SUCCESS_MESSAGE).text \
+               == self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text, \
+               "No product name in the message"
+
+    def should_be_right_basket_sum(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text \
+               == self.browser.find_element(*ProductPageLocators.BASKET_SUM).text, \
+               "Price not equal"
