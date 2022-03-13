8dcba073dbe4297ca267ece2c6b95226af4191ec
Zhanna <gorbacheva.zh.v@gmail.com>
2022-03-13 20:22:38 +0300

    Add negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -18,3 +18,12 @@ class ProductPage(BasePage):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text \
                == self.browser.find_element(*ProductPageLocators.BASKET_SUM).text, \
                "Price not equal"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message isn't disappeared"
+
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
 from .pages.product_page import ProductPage
 
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/"
+
 
 @pytest.mark.parametrize('link',
                          ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -22,3 +24,26 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_success_message()
     page.should_be_right_basket_sum()
+
+
+@pytest.mark.negative
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.negative
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.negative
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappeared_message()
